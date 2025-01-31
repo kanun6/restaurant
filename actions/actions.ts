768746925cfebb965c4273c47fad3b836a3ca4f6
kittayod <kanunrider.6762464@gmail.com>
2025-01-31 18:17:01 +0700

    add code & supabase

--- actions/actions.ts
+++ actions/actions.ts
@@ -3,6 +3,13 @@
 import { profileSchema, ValidateEithZode } from "@/utils/schemas";
 
 
+const renderError = (error: unknown):{ message :string} => {
+    return {
+        message: error instanceof Error ? error.message : 'An Error!!!'
+    }
+
+};
+
 export const createProfileAction = async (
   prevState: unknown,
   formData: FormData
@@ -14,7 +21,7 @@ export const createProfileAction = async (
     return { message: "Create Profile Success!!!" };
 
   } catch (error) {
-    console.log(error)
-    return { message: error.message  || 'An error server' }  
+    console.log(error);
+    return renderError(error) 
   }
 };
